ba57dc6e85932c9402615978aa4e17720650d33b
dev-shari <dev.sheharyar@gmail.com>
2026-07-07 13:04:25 +0500

    parking

---
+++ matching.py
@@ -0,0 +1,5 @@
+##### Parking Problem Solver
+print("Welcom to our Parking System.\nWe are Glade to have you here.")
+print("How can we help you? Select one from the following.")
+print("1. For new Parking ticket\n2. for checkout from parking\n3. for avalible space status.")
+ 
