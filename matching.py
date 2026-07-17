13cf28e1c3fd02db2829efe32086eb518d4d5996
dev-shari <dev.sheharyar@gmail.com>
2026-07-17 11:41:22 +0500

    up

--- matching.py
+++ matching.py
@@ -3,7 +3,12 @@ print("Welcom to our Parking System.\nWe are Glade to have you here.")
 print("How can we help you? Select one from the following.")
 print("1. For new Parking ticket\n2. for checkout from parking\n3. for avalible space status.\n4. for exit.")
 
-with open("geek.txt","a") as file:
+
+
+
+
+
+"""with open("geek.txt","a") as file:
     file.write("Hello Pakistan")
     print("good")
 
@@ -14,7 +19,7 @@ try:
 except FileNotFoundError as e:
     print("Error",e)
 finally:
-    file.close()
+    file.close()"""
 
 
 
